de5a4ab8cce904ec08ad9c6582ba7c0e39267a0c
JherikaSilva <pereirajherika@gmail.com>
2026-03-18 13:06:44 -0300

    feat: análise inicial de dados com cálculo de métricas estatísticas (NumPy)

---
+++ Estatistica/estatistica_basica.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+dados = [10, 20, 30, 40, 50]
+
+media = np.mean(dados)
+mediana = np.median(dados)
+desvio = np.std(dados)
+
+print("Média:", media)
+print("Mediana:", mediana)
+print("Desvio padrão:", desvio)
